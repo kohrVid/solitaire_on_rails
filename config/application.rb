15023226cdb28f30074324198c634b70219c857d
kohrVid <kohrVid@zoho.com>
2015-11-01 17:29:12 +0000

    Changes made to application.rb

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,7 @@ module Solitaire
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
+    Rails.application.config.action_controller.include_all_helpers = true
     config.active_record.raise_in_transactional_callbacks = true
     config.assets.initialize_on_precompile = true
   end
